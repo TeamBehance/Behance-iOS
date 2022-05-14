7c3a80d82cb7b3fc9d57a7444d61520212d10b84
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-05-15 02:49:27 +0900

    chore: #2 컬러에셋추가

---
+++ Behance-iOS/Behance-iOS/Configuration/Extensions/UiColor+.swift
@@ -0,0 +1,39 @@
+//
+//  UiColor+.swift
+//  Behance-iOS
+//
+//  Created by 김혜수 on 2022/05/15.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    @nonobjc class var behanceBlack: UIColor {
+        return UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var behanceDarkgray: UIColor {
+        return UIColor(red: 0.26, green: 0.26, blue: 0.29, alpha: 1.0)
+    }
+    
+    @nonobjc class var behanceGray: UIColor {
+        return UIColor(red: 0.63, green: 0.63, blue: 0.63, alpha: 1.0)
+    }
+    
+    @nonobjc class var behanceLightgray1: UIColor {
+        return UIColor(red: 0.7, green: 0.7, blue: 0.7, alpha: 1.0)
+    }
+    
+    @nonobjc class var behanceLightgray2: UIColor {
+        return UIColor(red: 0.94, green: 0.94, blue: 0.94, alpha: 1.0)
+    }
+    
+    @nonobjc class var behanceBlue: UIColor {
+        return UIColor(red: 0.0, green: 0.34, blue: 0.99, alpha: 1.0)
+    }
+    
+    @nonobjc class var behanceRed: UIColor {
+        return UIColor(red: 0.77, green: 0.27, blue: 0.26, alpha: 1.0)
+    }
+}
