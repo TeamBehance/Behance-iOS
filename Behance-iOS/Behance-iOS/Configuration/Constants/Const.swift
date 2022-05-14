6f6d165909926ad7a12c46fcb128a336ca740f35
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-05-15 02:35:39 +0900

    chore: #2 이미지 에셋 추가

---
+++ Behance-iOS/Behance-iOS/Configuration/Constants/Const.swift
@@ -0,0 +1,12 @@
+//
+//  Const.swift
+//  Behance-iOS
+//
+//  Created by 김혜수 on 2022/05/15.
+//
+
+import Foundation
+
+struct Const {
+    
+}
---
+++ Behance-iOS/Behance-iOS/Configuration/Constants/Image.swift
@@ -0,0 +1,41 @@
+//
+//  Image.swift
+//  Behance-iOS
+//
+//  Created by 김혜수 on 2022/05/15.
+//
+
+import UIKit
+
+extension Const {
+    
+    struct Image {
+        
+        static let acount = UIImage(named: "acount(ios)")!
+        static let bell = UIImage(named: "bell(ios)")!
+        static let cameraWhite = UIImage(named: "camera_white(ios)")!
+        static let camera = UIImage(named: "camera(ios)")!
+        static let cloud = UIImage(named: "cloud(ios)")!
+        static let code = UIImage(named: "code(ios)")!
+        static let grid = UIImage(named: "grid")!
+        static let home = UIImage(named: "home(ios)")!
+        static let play = UIImage(named: "play(ios)")!
+        static let search = UIImage(named: "search(ios)")!
+        static let text = UIImage(named: "text(ios)")!
+        static let tv = UIImage(named: "tv(ios)")!
+        
+        // Dummy
+        
+        static let imgDummy1 = UIImage(named: "imgDummy1")!
+        static let imgDummy2 = UIImage(named: "imgDummy2")!
+        static let imgDummy3 = UIImage(named: "imgDummy3")!
+        static let imgDummy4 = UIImage(named: "imgDummy4")!
+        static let imgDummy5 = UIImage(named: "imgDummy5")!
+        static let imgDummy6 = UIImage(named: "imgDummy6")!
+        static let imgDummy7 = UIImage(named: "imgDummy7")!
+        static let imgDummy8 = UIImage(named: "imgDummy8")!
+        static let imgDummy9 = UIImage(named: "imgDummy9")!
+        static let imgDummy10 = UIImage(named: "imgDummy10")!
+        static let imgDummy11 = UIImage(named: "imgDummy11")!
+    }
+}
