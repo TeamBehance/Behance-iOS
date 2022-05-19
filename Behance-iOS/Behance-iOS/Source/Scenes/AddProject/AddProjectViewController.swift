bef35ad86ce59c81b4cf7dd10adea7f776001f91
inuinseoul <inwoo7233@naver.com>
2022-05-20 00:15:47 +0900

    feat: #4 프로젝트 메뉴바 투명화처리 및 애니메이션 제거

--- Behance-iOS/Behance-iOS/Source/Scenes/AddProject/AddProjectViewController.swift
+++ Behance-iOS/Behance-iOS/Source/Scenes/AddProject/AddProjectViewController.swift
@@ -27,22 +27,19 @@ final class AddProjectViewController: UIViewController {
                 editorHeightConstraint.constant += translation.y
             }
         case .ended:
-            print(sender.velocity(in: view))
-            UIView.animate(withDuration: 1000) {
-                let percentage = (self.editorCollectionView.frame.height + translation.y) / self.view.frame.height
-                if percentage > 0.8 {
-                    self.editorHeightConstraint.constant = self.view.frame.height*0.35 - 120
-                } else if 0.4 < percentage && percentage < 0.8 {
-                    self.editorHeightConstraint.constant = 0
-                } else {
-                    self.editorHeightConstraint.constant = -self.view.frame.height*0.5
-                }
+            let percentage = (self.editorCollectionView.frame.height + translation.y) / self.view.frame.height
+            if percentage > 0.8 {
+                self.editorHeightConstraint.constant = self.view.frame.height*0.20
+            } else if 0.4 < percentage && percentage < 0.8 {
+                self.editorHeightConstraint.constant = 0
+            } else {
+                self.editorHeightConstraint.constant = -self.view.frame.height*0.5
             }
         default:
             break
         }
         
-        var opacity = (view.frame.height*0.35 - 120 - editorHeightConstraint.constant) / 100
+        var opacity = (self.view.frame.height*0.20 - editorHeightConstraint.constant) / 100
         opacity = opacity < 0 ? 0 : opacity
         opacity = opacity > 1 ? 1 : opacity
         
