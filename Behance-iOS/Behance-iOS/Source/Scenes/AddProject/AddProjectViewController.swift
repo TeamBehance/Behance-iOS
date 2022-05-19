6236c89547092807290f5e32dae862fcbd60e457
inuinseoul <inwoo7233@naver.com>
2022-05-20 00:02:52 +0900

    feat: #4 기본 UI 요소 추가 및 Gesture 추가

--- Behance-iOS/Behance-iOS/Source/Scenes/AddProject/AddProjectViewController.swift
+++ Behance-iOS/Behance-iOS/Source/Scenes/AddProject/AddProjectViewController.swift
@@ -9,21 +9,43 @@ import UIKit
 
 final class AddProjectViewController: UIViewController {
 
+    @IBOutlet weak var editorCollectionView: UICollectionView!    
+    @IBOutlet weak var menuStackView: UIStackView!
+    @IBOutlet weak var photoCollectionView: UICollectionView!
+    @IBOutlet weak var editorHeightConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func menuDidMoved(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        sender.setTranslation(.zero, in: view)
+        switch sender.state {
+        case .changed:
+            if editorHeightConstraint.constant >= -view.frame.height*0.5 {
+                editorHeightConstraint.constant += translation.y
+            }
+        case .ended:
+            print(sender.velocity(in: view))
+            UIView.animate(withDuration: 1000) {
+                let percentage = (self.editorCollectionView.frame.height + translation.y) / self.view.frame.height
+                if percentage > 0.8 {
+                    self.editorHeightConstraint.constant = self.view.frame.height*0.35 - 120
+                } else if 0.4 < percentage && percentage < 0.8 {
+                    self.editorHeightConstraint.constant = 0
+                } else {
+                    self.editorHeightConstraint.constant = -self.view.frame.height*0.5
+                }
+            }
+        default:
+            break
+        }
+        
+        var opacity = (view.frame.height*0.35 - 120 - editorHeightConstraint.constant) / 100
+        opacity = opacity < 0 ? 0 : opacity
+        opacity = opacity > 1 ? 1 : opacity
+        
+        photoCollectionView.alpha = opacity
     }
-    */
-
 }
