8807fc1f13f1e1796c98efbee8637a719869f165
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-05-24 17:06:15 +0900

    [ADD] 파일 추가

---
+++ Behance-iOS/Behance-iOS/Source/Scenes/Home/Cells/storyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  storyTableViewCell.swift
+//  Behance-iOS
+//
+//  Created by 강윤서 on 2022/05/24.
+//
+
+import UIKit
+
+class storyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
