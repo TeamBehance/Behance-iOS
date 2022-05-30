73ae9cfc5aa52daf6c437dbff6bd2e3e86b7c9ee
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-05-31 02:18:54 +0900

    chore: #8 kingfisher extension 추가

---
+++ Behance-iOS/Behance-iOS/Configuration/Extensions/UIImageView+.swift
@@ -0,0 +1,30 @@
+//
+//  UIImageView+.swift
+//  Behance-iOS
+//
+//  Created by 김혜수 on 2022/05/31.
+//
+
+import Foundation
+
+import Kingfisher
+
+extension UIImageView {
+    func setImage(with urlString: String) {
+        let cache = ImageCache.default
+        cache.retrieveImage(forKey: urlString, options: nil) { result in // 캐시에서 키를 통해 이미지를 가져온다.
+            switch result {
+            case .success(let value):
+                if let image = value.image { // 만약 캐시에 이미지가 존재한다면
+                    self.image = image // 바로 이미지를 셋한다.
+                } else {
+                    guard let url = URL(string: urlString) else { return }
+                    let resource = ImageResource(downloadURL: url, cacheKey: urlString) // URL로부터 이미지를 다운받고 String 타입의 URL을 캐시키로 지정하고
+                    self.kf.setImage(with: resource) // 이미지를 셋한다.
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
