72685cc2bca3350a1df4f72952642cbcefc336a6
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-06-03 15:59:04 +0900

    [FEAT] 프로젝트 조회 서비스코드 작성

--- Behance-iOS/Behance-iOS/Source/Scenes/Home/Services/HomeService.swift
+++ Behance-iOS/Behance-iOS/Source/Scenes/Home/Services/HomeService.swift
@@ -36,10 +36,44 @@ class HomeService {
         }
     }
     
+    func project(completion: @escaping (NetworkResult<Any>) -> Void)
+    {
+        let url =  "\(URLConstants.baseURL)\(URLConstants.project)"
+        let header: HTTPHeaders = ["Content-Type" : "application/json"]
+
+        let dataRequest = AF.request(url,
+                                     method: .get,
+                                     encoding: JSONEncoding.default,
+                                     headers: header)
+
+        dataRequest.responseData { response in
+            switch response.result {
+            case .success:
+                guard let statusCode = response.response?.statusCode else { return }
+                guard let value = response.value else { return }
+                let networkResult = self.judgeProjectStatus(by: statusCode, value)
+                completion(networkResult)
+
+            case .failure:
+                completion(.networkFail)
+            }
+        }
+    }
+    
     private func judgeStoryStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
         switch statusCode {
         // 성공 시에는 넘겨받은 데이터를 decode(해독)하는 함수를 호출합니다.
         case 200..<300: return isVaildStoryData(data: data)
+        case 400..<500: return .pathErr
+        case 500: return .serverErr
+        default: return .networkFail
+        }
+    }
+    
+    private func judgeProjectStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        switch statusCode {
+        // 성공 시에는 넘겨받은 데이터를 decode(해독)하는 함수를 호출합니다.
+        case 200..<300: return isVaildProjectData(data: data)
         case 400..<500: return .pathErr
         case 500: return .serverErr
         default: return .networkFail
@@ -56,5 +90,12 @@ class HomeService {
         return .success(decodedData as Any)
     }
     
+    private func isVaildProjectData(data: Data) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode(ProjectResponse.self, from: data)
+        else { return .pathErr }
+        
+        return .success(decodedData as Any)
+    }
 }
 
